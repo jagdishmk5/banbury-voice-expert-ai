2d5e00f1bb14e9de3905bd689f438014c46eb6a4
jagdishmk5 <JAGDISHMK5@GMAIL.COM>
2026-09-25 19:17:56 +0530

    Add dictionary V1
    Added various hardness and material properties to the dictionary.

--- dictionary.js
+++ dictionary.js
@@ -1,1 +1,52 @@
-// Banbury Dictionary Engine
+const dictionary = {
+
+hardness_high: [
+"compound hard",
+"material hard",
+"rubber hard",
+"hardness high",
+"sheet hard"
+],
+
+hardness_low: [
+"compound soft",
+"material soft",
+"rubber soft",
+"soft compound",
+"sheet soft"
+],
+
+specific_gravity: [
+"specific gravity",
+"sg",
+"density",
+"heavy compound",
+"compound heavy"
+],
+
+mooney_high: [
+"mooney high",
+"viscosity high",
+"extruder load high",
+"material flow problem",
+"compound process problem"
+],
+
+dispersion_poor: [
+"black dots",
+"compound lumpy",
+"material lumpy",
+"rubber lumpy",
+"dispersion poor",
+"carbon mix nahi hua"
+],
+
+rheometer: [
+"rheo fail",
+"rheometer fail",
+"mdr fail",
+"cure problem",
+"cure out"
+]
+
+};
