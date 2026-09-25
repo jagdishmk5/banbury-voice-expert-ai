8943bd789d56e0486e71422b23663e924b11b793
jagdishmk5 <JAGDISHMK5@GMAIL.COM>
2026-09-26 00:33:34 +0530

    Simplify getBanburyAnswer function to return status

--- frontend/engine.js
+++ frontend/engine.js
@@ -1,26 +1,3 @@
-function getBanburyAnswer(question) {
-
-  if (!question) {
-    return "Please enter a question";
-  }
-
-  question = question.toLowerCase();
-
-  for (const topic in dictionary) {
-
-    for (const phrase of dictionary[topic]) {
-
-      if (question.includes(phrase.toLowerCase())) {
-
-        if (knowledgeBase[topic]) {
-          return knowledgeBase[topic];
-        }
-
-      }
-
-    }
-
-  }
-
-  return "PROBLEM:\nNot Identified\n\nMESSAGE:\nBanbury AI could not identify the problem.\n\nCONFIDENCE:\n0%;
+function getBanburyAnswer() {
+  return "ENGINE OK";
 }
