1ac3aae484931c66cf65ce2d4939fd79cac36667
jagdishmk5 <JAGDISHMK5@GMAIL.COM>
2026-09-25 23:13:17 +0530

    Add dictionary.js with material properties terms

---
+++ frontend/dictionary.js
@@ -0,0 +1,239 @@
+const dictionary = {
+
+hardness_high: [
+"compound hard",
+"material hard",
+"rubber hard",
+"hardness high",
+"sheet hard",
+"hard compound",
+"compound kadak",
+"rubber kadak"
+],
+
+hardness_low: [
+"compound soft",
+"material soft",
+"rubber soft",
+"soft compound",
+"sheet soft",
+"rubber naram",
+"compound naram"
+],
+
+specific_gravity: [
+"specific gravity",
+"sg",
+"density",
+"heavy compound",
+"compound heavy",
+"density high",
+"sg high",
+"specific gravity high"
+],
+
+mooney_high: [
+"mooney high",
+"viscosity high",
+"extruder load high",
+"material flow problem",
+"compound process problem",
+"flow problem",
+"process problem",
+"high viscosity"
+],
+
+mooney_low: [
+"mooney low",
+"viscosity low",
+"material very soft",
+"rubber flow high"
+],
+
+dispersion_poor: [
+"black dots",
+"compound lumpy",
+"material lumpy",
+"rubber lumpy",
+"dispersion poor",
+"carbon mix nahi hua",
+"cb not mixed",
+"mixing poor"
+],
+
+rheometer: [
+"rheo fail",
+"rheometer fail",
+"mdr fail",
+"cure problem",
+"cure out",
+"cure issue"
+],
+
+torque_high: [
+"torque high",
+"final torque high",
+"maximum torque high",
+"torque jyada"
+],
+
+torque_low: [
+"torque low",
+"final torque low",
+"minimum torque low",
+"torque kam"
+],
+
+dump_temperature: [
+"dump temperature",
+"low dump temperature",
+"high dump temperature",
+"dump temp"
+],
+
+carbon_black: [
+"carbon black",
+"carbon high",
+"carbon low",
+"cb high",
+"cb low",
+"n220",
+"n234",
+"n330",
+"n339",
+"n375",
+"n550",
+"n660"
+],
+
+process_oil: [
+"process oil",
+"oil high",
+"oil low",
+"tdae oil",
+"aromatic oil"
+],
+
+ram_pressure: [
+"ram pressure",
+"ram low",
+"ram high"
+],
+
+mixing_time: [
+"mixing time",
+"mixing low",
+"mixing high",
+"mixing issue"
+],
+
+rotor_speed: [
+"rotor speed",
+"rotor rpm",
+"rpm high",
+"rpm low"
+],
+
+rotor_wear: [
+"rotor wear",
+"rotor damaged",
+"rotor issue"
+],
+
+load_cell: [
+"load cell",
+"weighment issue",
+"wrong weighing",
+"weight variation"
+],
+
+oil_pump: [
+"oil pump",
+"oil flow issue",
+"oil flow low"
+],
+
+temperature_sensor: [
+"temperature sensor",
+"sensor issue",
+"temp sensor"
+],
+
+hydraulic_pressure: [
+"hydraulic pressure",
+"hydraulic low",
+"hydraulic issue"
+],
+
+scorch: [
+"scorch",
+"scorch high",
+"scorch low"
+],
+
+tensile: [
+"tensile",
+"tensile low",
+"tensile high"
+],
+
+elongation: [
+"elongation",
+"elongation low",
+"elongation high"
+],
+
+ash: [
+"ash",
+"ash high",
+"ash low"
+],
+
+sulphur: [
+"sulphur",
+"sulfur",
+"sulphur high",
+"sulphur low"
+],
+
+accelerator: [
+"accelerator",
+"cbs",
+"tbbs",
+"tmtd",
+"dpg"
+],
+
+nr: [
+"nr",
+"natural rubber"
+],
+
+sbr: [
+"sbr"
+],
+
+br: [
+"br",
+"butadiene rubber"
+],
+
+batch_hold: [
+"batch hold",
+"compound hold",
+"hold batch"
+],
+
+rework: [
+"rework",
+"reprocessing",
+"reworked batch"
+],
+
+scrap: [
+"scrap",
+"scrapped batch",
+"rejected batch"
+]
+ 
+};
