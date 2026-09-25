58b32f772cef751321d5acb6b3b42bdafa576fcd
jagdishmk5 <JAGDISHMK5@GMAIL.COM>
2026-09-25 20:48:16 +0530

    Refactor getBanburyAnswer function to return dictionary keys
    Removed the getBanburyAnswer function and replaced it with a return statement that outputs the keys of the dictionary as a JSON string.

--- engine.js
+++ engine.js
@@ -1,36 +1,5 @@
 function getBanburyAnswer(question){
-
-if(!question){
-return "Please enter a question";
-}
-
-question = question.toLowerCase();
-
-for(const topic in dictionary){
-
-for(const phrase of dictionary[topic]){
-
-if(question.includes(phrase.toLowerCase())){
-
-if(knowledgeBase[topic]){
-return knowledgeBase[topic];
-}
-
-}
-
-}
-
-}
-
-return `
-PROBLEM:
-Not Identified
-
-MESSAGE:
-Banbury AI could not identify the problem.
-
-CONFIDENCE:
-0%
-`;
-
+ 
+return JSON.stringify(Object.keys(dictionary));
+ 
 }
