dcbaf2982ce1e2412930f30bf7d39c2932d06481
jagdishmk5 <JAGDISHMK5@GMAIL.COM>
2026-09-25 19:21:14 +0530

    Connect Dictionary and Knowledge Base
    Add function to get answers from Banbury AI based on user questions.

--- engine.js
+++ engine.js
@@ -1,1 +1,39 @@
-// Banbury AI Engine
+function getBanburyAnswer(question){
+
+question = question.toLowerCase();
+
+for(const topic in dictionary){
+
+for(const phrase of dictionary[topic]){
+
+if(question.includes(phrase.toLowerCase())){
+
+return knowledgeBase[topic];
+
+}
+
+}
+
+}
+
+return `
+PROBLEM:
+Not Identified
+
+MESSAGE:
+Banbury AI could not identify the problem.
+
+Try describing:
+
+- Hard compound
+- Soft compound
+- Density high
+- Mooney high
+- Black dots
+- Rheo fail
+
+CONFIDENCE:
+0%
+`;
+
+}
